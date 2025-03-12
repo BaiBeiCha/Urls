509a8227147b9912a3e9f6f85d45c3784b65a7c5
baibeicha <myvvot@gmail.com>
2025-03-12 14:28:37 +0300

    update

--- src/main/java/com/baibei/urls/controllers/mvc/RedirectController.java
+++ src/main/java/com/baibei/urls/controllers/mvc/RedirectController.java
@@ -2,7 +2,9 @@ package com.baibei.urls.controllers.mvc;
 
 import com.baibei.urls.services.UrlService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -12,11 +14,16 @@ public class RedirectController {
 
     private final UrlService urlService;
 
+    @Value("${urls.link-redirect-time}")
+    private long linkRedirectTime;
+
     @GetMapping("/{shortId}")
-    public String redirectToShortUrl(@PathVariable String shortId) {
+    public String redirectToShortUrl(@PathVariable String shortId, Model model) {
         String fullUrl = urlService.getFullUrl(shortId);
         if (fullUrl != null && !fullUrl.isEmpty()) {
-            return "redirect:" + fullUrl;
+            model.addAttribute("targetUrl", fullUrl);
+            model.addAttribute("waitTime", linkRedirectTime);
+            return "redirect";
         }
         return "redirect:/error/404";
     }
